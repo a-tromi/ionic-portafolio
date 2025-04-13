defccdf62dd102dbe71392dccbda33f0805364c7
a-tromi <148511515+a-tromi@users.noreply.github.com>
2025-04-12 21:40:40 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'appPatitasPet',
+  webDir: 'www'
+};
+
+export default config;
