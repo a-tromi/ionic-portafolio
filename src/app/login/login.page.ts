95d2c1a40965bf8b39fd44a1a9d40eb20b20ec29
a-tromi <148511515+a-tromi@users.noreply.github.com>
2025-04-19 02:22:43 -0400

    Creación Login

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  standalone: true,
+  imports: [IonicModule],
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
