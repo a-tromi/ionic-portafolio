454f250b7078e215025eb09b5a4bab68eccc7a5d
a-tromi <148511515+a-tromi@users.noreply.github.com>
2025-04-21 18:50:37 -0400

    Menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,6 +16,8 @@ const routes: Routes = [
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
+  
+  
 ];
 
 @NgModule({
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MenuController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +9,14 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class AppComponent {
-  constructor() {}
+  constructor(
+    private menu: MenuController, 
+    private router: Router
+  ) {}
+
+  cerrarSesion(): void {
+    console.log('Sesión cerrada');
+    this.menu.close('mainMenu');
+    this.router.navigate(['/login']);
+  }
 }
